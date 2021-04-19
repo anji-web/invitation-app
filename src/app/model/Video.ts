e5d78215366fde98e1947823147277888a36b9ca
anji-web <56215241+anji-web@users.noreply.github.com>
2021-04-19 13:44:41 +0700

    Auto-generated commit

--- src/app/model/Video.ts
+++
@@ -1,8 +0,0 @@
-export class Video {
-  constructor(
-    public videoId: number ,
-    public data: Blob ,
-    public fileName: string,
-    public fileType: string,
-    public videoTitle: string) {}
-}
